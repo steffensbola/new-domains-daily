3100446538a9cd2e3e7f399c40c9eaca5dbf79b7
cristiano.steffens <cristiano.steffens@clear.sale>
2022-08-08 15:53:04 -0300

    Updated files via script

---
+++ .github/workflows/run_cron.sh
@@ -0,0 +1,36 @@
+echo "Starting..."
+cd ../../
+git fetch
+git checkout main
+git pull
+
+echo "Installing dependencies..."
+npm i
+echo "Installing dependencies... Done"
+
+echo "Running scripts..."
+npx ts-node src/main.ts -y
+## alternative way:
+# tsc src/main.ts
+# node src/main.js
+echo "Running scripts... Done"
+
+echo "Unzipping..."
+cd data
+while read -r line; do (unzip -o -d "$(basename "$line" .zip)" "$line"); done < <(find . | grep '.zip')
+
+echo "Removing temporary Zip files..."
+rm *.zip
+
+date --iso-8601=seconds > last_update.txt
+cd ..
+echo "Done!"
+
+
+echo "Updating repo..."
+
+git add -A && git commit -m "Updated files via script" 
+git push
+echo "Updating repo... Done"
+
+echo "Press any key to exit..."
